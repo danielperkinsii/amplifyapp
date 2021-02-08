bd1b6a316c8060ce88791e8f21b56b561074888e
Daniel Perkins II <daniel.perkins99@gmail.com>
2021-02-08 14:03:38 -0600

    Styling and layout improvements

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import ImageIcon from './svg/si-sprite.svg'
 
 const initialFormState = { name: '', description: ''};
-
+const stickyColors = ['Orange', 'Red', 'Green', 'Purple', 'Blue']
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState)
-
+ 
 
   useEffect(() => {
     fetchNotes();
@@ -58,20 +59,45 @@ function App() {
   return (
     <div className="App">
       <h1>My Notes App</h1>
-      <input onChange={e => setFormData({ ...formData, 'name': e.target.value })} placeholder="Note name" value={formData.name}/><br/>
-      <input onChange={e => setFormData({ ...formData, 'description': e.target.value})} placeholder="Note description" value={formData.description}/> <br/>
-      <button onClick={createNote}>Create Note</button><br/>
-      <input type="file" onChange={handleChange}/>
-      <div style={{marginBottom: 30}}>
+      <div className="Note-form">
+      <input
+       className="Field" 
+       onChange={e => setFormData({ ...formData, 'name': e.target.value })} 
+       placeholder="Note name" 
+       value={formData.name}/><br/>
+      <textarea
+       className="Field"
+      rows="5"
+      cols="50"
+      onChange={e => setFormData({ ...formData, 'description': e.target.value})} 
+      placeholder="Note description" 
+      value={formData.description}></textarea> <br/>
+       <label className="Button-space Upload-style Upload">
+       Image
+       <input 
+       type="file" onChange={handleChange}/>
+       </label><br/>
+       <button 
+       className="Button-space Button-style Save"
+       onClick={createNote}>Save</button><br/>
+       </div>
+      
+      <div className="Note-container">
         {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              {
-                note.image && <img src={note.image} style={{width: 400}}/>
-              }
-              <button onClick={() => deleteNote(note)}>Delete note</button>
+          notes.map((note, index) => (
+            <div className={`Note ${stickyColors[index % stickyColors.length]}`} key={note.id || note.name}>
+              <div className="Note-header">
+                <h2 className="Note-title">{note.name}</h2>
+                {
+                  note.image && <img className="Note-image-thumbnail" src={note.image}/>
+                }
+              </div>
+              <p className="Note-body">{note.description}</p>
+              <div className="Note-utilities">
+              <button
+               className="Button-space Button-style Delete"
+               onClick={() => deleteNote(note)}>Delete note</button>
+              </div>
             </div>
           ))
         }
